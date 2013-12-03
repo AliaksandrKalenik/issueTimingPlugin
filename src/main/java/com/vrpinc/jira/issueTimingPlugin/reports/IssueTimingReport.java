00bf79a76800f327b906bb2657cdcaabcc160240
a.kalenik <a.kalenik94@gmail.com>
2013-12-04 00:48:20 +0200

    Added worklog for issues

--- src/main/java/com/vrpinc/jira/issueTimingPlugin/reports/IssueTimingReport.java
+++ src/main/java/com/vrpinc/jira/issueTimingPlugin/reports/IssueTimingReport.java
@@ -1,10 +1,15 @@
 package com.vrpinc.jira.issueTimingPlugin.reports;
 
 import com.atlassian.query.Query;
+import com.atlassian.core.util.map.EasyMap;
+import com.atlassian.jira.ComponentManager;
 import com.atlassian.jira.bc.issue.search.SearchService;
+import com.atlassian.jira.exception.PermissionException;
 import com.atlassian.jira.issue.Issue;
 import com.atlassian.jira.issue.search.SearchException;
 import com.atlassian.jira.issue.search.SearchResults;
+import com.atlassian.jira.issue.worklog.Worklog;
+import com.atlassian.jira.issue.worklog.WorklogManager;
 import com.atlassian.jira.jql.builder.JqlQueryBuilder;
 import com.atlassian.jira.plugin.report.impl.AbstractReport;
 import com.atlassian.jira.security.JiraAuthenticationContext;
@@ -15,6 +20,7 @@ import com.opensymphony.util.TextUtils;
 import org.apache.log4j.Logger;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -25,28 +31,57 @@ public class IssueTimingReport extends AbstractReport
     private static final Logger log = Logger.getLogger(IssueTimingReport.class);
     private final JiraAuthenticationContext authenticationContext;
     private final SearchService searchService;
+    private Map params;
     private final String issueTimespentNull = "0s";
+    private final String issuePathUrl = "/browse/";
+	private WorklogManager worlogManager;
     public IssueTimingReport(final JiraAuthenticationContext authenticationContext,
-                                            final SearchService searchService)
+                                            final SearchService searchService, WorklogManager worklogManager)
     {
+    	this.worlogManager = worklogManager;
         this.authenticationContext = authenticationContext;
         this.searchService = searchService;
     }
 
-	public String generateReportHtml(ProjectActionSupport projectActionSupport, Map params){
+	public String generateReportHtml(ProjectActionSupport projectActionSupport, Map params) throws PermissionException{
+		this.params = params;
 		Long currentProjectId = getCurrentProjectId(params);
 		List<Issue> issues = this.getAllIssuesInCurrentProject(currentProjectId);
-		String message = new String(issuesToTable(issues));
-    	return message;
+		Long issueId = getSelectedIssueId(params);
+		List<Worklog> worklogs = null;		
+		if(issueId != -1){
+			//worklogs = getWorklog(issues.get(0));
+			worklogs = getWorklog(issues, issueId);
+		}
+		final Map startingParams;
+		startingParams = EasyMap.build(
+                "projectId", currentProjectId,
+				"issues", issues,
+                "issueId", issueId,
+                "worklogs", worklogs,
+                "th", this); 
+		return descriptor.getHtml("view", startingParams);
     	
     }
+
+
+
+	private Long getSelectedIssueId(Map params) {
+		Long issueId;
+		try{
+			issueId = Long.valueOf(TextUtils.htmlEncode((String) params.get("issueId")));
+		}catch(Exception e){
+			issueId = (long) -1;
+		}
+		return issueId;
+	}
 
 	private Long getCurrentProjectId(Map params) {
 		Long currentProjectId = Long.valueOf(TextUtils.htmlEncode((String) params.get("selectedProjectId")));
 		return currentProjectId;
 	}
 	
-    private String formatTime(final long sec) {
+    public String formatTime(final long sec) {
 		String result ="";
     	final int secInMinute = 60;
 		final int secInHour = 3600;
@@ -99,31 +134,73 @@ public class IssueTimingReport extends AbstractReport
 	private String issuesToTable(List<Issue> issues){
 		String tableHtml = "<table border =\"2\"  align=\"center\" cellpadding=\"7\" cellspacing=\"7\">"
 				+ "<tr>"
+			+"<th align=\"left\">Worklog</th>"
 			+"<th align=\"left\">Time spent</th>"
 			+"<th align=\"left\">Issue Summary</th>"
 		+"</tr>";
 		Iterator<Issue> it = issues.iterator();
     	while(it.hasNext()){
     		Issue current = it.next();
+    		String link = getIssuesPathUrl(current);
+    		List<Worklog> worklog = getWorklog(current);
+    		link = worklog.get(0).getUpdated().toString();
+    		worklog.get(0).getUpdateAuthorFullName();
 	    	if(current.getTimeSpent()!=null){
 	    		long sec = current.getTimeSpent();
 	    		String result = formatTime(sec);
-	    		tableHtml = tableHtml.concat(addRawToTable(current.getSummary(), result));
+	    		tableHtml = tableHtml.concat(addRawToTable(link,current.getSummary(), result));
     		}
 	    	else{
-	    		tableHtml = tableHtml.concat(addRawToTable(current.getSummary(), this.issueTimespentNull));
+	    		tableHtml = tableHtml.concat(addRawToTable(link, current.getSummary(), this.issueTimespentNull));
 	    	}
     	}
     	tableHtml = tableHtml.concat("</table>");
 		return tableHtml;
 	}
+	
+	public Issue getIssueById(List<Issue> issues,Long issueId){
+		Iterator<Issue> issueIterator = issues.iterator();
+		while(issueIterator.hasNext()){
+			Issue current = issueIterator.next();
+			if(current.getId().equals(issueId)){
+				return current;
+			}
+		}
+		return null;
+	}
+	
+	private List<Worklog> getWorklog(List<Issue> issues, Long issueId) {
+		Issue issue = this.getIssueById(issues, issueId);
+		return this.getWorklog(issue);
+	}
+	
+	private List<Worklog> getWorklog(Issue issue) {
+		List<Worklog> worklog = worlogManager.getByIssue(issue);
+		return worklog;
+	}
+	
+	public String getIssueWorklogReportURL(Long projectId, Long issueId){
+		String url = ComponentManager.getInstance().getApplicationProperties().getString("jira.baseurl");
+		url = url.concat("/secure/ConfigureReport.jspa?selectedProjectId="+projectId+"&reportKey=com.vrpinc.jira.issueTimingPlugin%3Aissue-timing-report&Next=Next");
+		url = url.concat("&issueId="+issueId.toString());
+		return url;
+	}
+	
+	public String getIssuesPathUrl(Issue issue) {
+		String link = ComponentManager.getInstance().getApplicationProperties().getString("jira.baseurl");
+		link = link.concat(this.issuePathUrl);
+		link = link.concat(issue.getKey());
+		return link;
+	}
 
-	private String addRawToTable(String summary, String result) {
+	private String addRawToTable(String link, String summary, String result) {
 		String rawHtml = "";
 		rawHtml = rawHtml.concat("<tr>");
+		rawHtml = rawHtml.concat("<td >"+link+"</td>");
 		rawHtml = rawHtml.concat("<td >"+result+"</td>");
-		rawHtml = rawHtml.concat("<td >"+summary+"</td>");
+		rawHtml = rawHtml.concat("<td ><a href=\""+link+"\" target=\"_blank\">"+summary+"</a></td>");
 		rawHtml = rawHtml.concat("</tr>");
 		return rawHtml;
 	}
+
 }
